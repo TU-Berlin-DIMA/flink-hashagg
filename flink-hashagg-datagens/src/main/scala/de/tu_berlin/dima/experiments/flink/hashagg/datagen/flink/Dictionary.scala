9b9883a71ddf9263591d5d8f2e73d8a45d9a7627
Alexander Alexandrov <alexander.s.alexandrov@gmail.com>
2016-03-23 15:05:41 +0100

    Added KeyValuePairsGenerator.

--- flink-hashagg-datagens/src/main/scala/de/tu_berlin/dima/experiments/flink/hashagg/datagen/flink/Dictionary.scala
+++ flink-hashagg-datagens/src/main/scala/de/tu_berlin/dima/experiments/flink/hashagg/datagen/flink/Dictionary.scala
@@ -4,18 +4,18 @@ import de.tu_berlin.dima.experiments.flink.hashagg.datagen.util.RanHash
 
 class Dictionary (seed : Long, size : Int) extends Iterable[String] {
 
-  private val MIN_LENGTH = 2
-  private val MAX_LENGTH = 16
-  private val NUM_CHARACTERS = 26
+  val MIN_LENGTH = 10
+  val MAX_LENGTH = 20
+  val NUM_CHARACTERS = 26
 
   private val random : RanHash = new RanHash(seed)
 
-  override def iterator: Iterator[String] = words.iterator
+  override def iterator: Iterator[String] = words().iterator
 
   def word (index : Int) : String = {
     require(0 <= index && index < size)
     // skip to the correct position within the random sequence
-    random.skipTo(index * (NUM_CHARACTERS + 1))
+    random.skipTo(index * MAX_LENGTH)
     val len = random.nextInt(MAX_LENGTH - MIN_LENGTH - 1) + MIN_LENGTH
     val strBld = new StringBuilder(len)
     for (i <- 0 until len) {
---
+++ flink-hashagg-datagens/src/main/scala/de/tu_berlin/dima/experiments/flink/hashagg/datagen/flink/KeyValuePairsGenerator.scala
@@ -0,0 +1,70 @@
+package de.tu_berlin.dima.experiments.flink.hashagg.datagen.flink
+
+import de.tu_berlin.dima.experiments.flink.hashagg.datagen.flink.Distributions.{Binomial, DiscreteDistribution, DiscreteUniform, Zipf}
+import de.tu_berlin.dima.experiments.flink.hashagg.datagen.util.RanHash
+import org.apache.flink.api.scala._
+import org.apache.flink.core.fs.FileSystem
+import org.apache.flink.util.NumberSequenceIterator
+
+object KeyValuePairsGenerator {
+
+  val SEED = 0xC00FFEE
+  val SIZE_OF_DICTIONARY = 1000000
+
+  val valDistribution = DiscreteUniform(SIZE_OF_DICTIONARY)
+
+  def main(args: Array[String]): Unit = {
+
+    if (args.length != 5) {
+      Console.err.println("Usage: <jar> numberOfTasks tuplesPerTask key-cardinality key-distribution[params] outputPath")
+      System.exit(-1)
+    }
+
+    val numberOfTasks   = args(0).toInt
+    val tuplesPerTask   = args(1).toLong
+    val keyCardinality  = args(2).toInt
+    val keyDistribution = parseDist(keyCardinality, args(3))
+    val outputPath      = args(4)
+
+    val numberOfPairs   = numberOfTasks * tuplesPerTask
+
+    // generate dictionary of random words
+    val dictionary = new Dictionary(SEED, SIZE_OF_DICTIONARY).words()
+
+    val environment = ExecutionEnvironment.getExecutionEnvironment
+
+    environment
+      // create a sequence [1 .. N] to create N words
+      .fromParallelCollection(new NumberSequenceIterator(1, numberOfPairs))
+      // set up workers
+      .setParallelism(numberOfTasks)
+      // map every n <- [1 .. N] to a random word sampled from a word list and a key
+      .map(i => {
+        val r = new RanHash(SEED + 2 * i)
+        val k = keyDistribution.sample(r.next())
+        val v = dictionary(valDistribution.sample(r.next()))
+        s"$k,$v"
+      })
+      .setParallelism(numberOfTasks)
+      // write result to file
+      .writeAsText(outputPath, FileSystem.WriteMode.OVERWRITE)
+
+    environment.execute(s"KeyValuePairsGenerator[$numberOfPairs]")
+  }
+
+  object Patterns {
+    val DiscreteUniform = """(Uniform)""".r
+    val Binomial = """Binomial\[(1|1\.0|0\.\d+)\]""".r
+    val Zipf = """Zipf\[(\d+(?:\.\d+)?)\]""".r
+  }
+
+  def parseDist(card: Int, s: String): DiscreteDistribution = s match {
+    case Patterns.DiscreteUniform(_) =>
+      DiscreteUniform(card)
+    case Patterns.Binomial(a) =>
+      Binomial(card, a.toDouble)
+    case Patterns.Zipf(a) =>
+      Zipf(card, a.toDouble)
+  }
+
+}
